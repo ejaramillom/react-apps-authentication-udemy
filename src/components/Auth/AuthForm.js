7f03d9252e3ebf3bc91ee54fb7ab0e4cb53eed97
Emmanuel <ejaramilo@hotmail.com>
2022-11-17 10:38:08 -0500

    307 managing the auth state with context

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,10 +1,14 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useContext } from 'react';
 
-import classes from "./AuthForm.module.css";
+import AuthContext from '../../store/auth-context';
+import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
+
+  const authCtx = useContext(AuthContext);
+
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -18,8 +22,9 @@ const AuthForm = () => {
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
 
-    setIsLoading(true);
+    // optional: Add validation
 
+    setIsLoading(true);
     let url;
 
     if (isLogin) {
@@ -31,26 +36,25 @@ const AuthForm = () => {
     }
 
     fetch(url, {
-      method: "POST",
+      method: 'POST',
       body: JSON.stringify({
         email: enteredEmail,
         password: enteredPassword,
         returnSecureToken: true,
       }),
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     })
       .then((res) => {
         setIsLoading(false);
-
         if (res.ok) {
-          return res.json;
+          return res.json();
         } else {
-          res.json().then((data) => {
-            let errorMessage = "Authentication failed!";
+          return res.json().then((data) => {
+            let errorMessage = 'Authentication failed!';
             // if (data && data.error && data.error.message) {
-            //   const errorMessage = data.error.message;
+            //   errorMessage = data.error.message;
             // }
 
             throw new Error(errorMessage);
@@ -58,7 +62,7 @@ const AuthForm = () => {
         }
       })
       .then((data) => {
-        console.log(data);
+        authCtx.login(data.idToken);
       })
       .catch((err) => {
         alert(err.message);
@@ -67,32 +71,32 @@ const AuthForm = () => {
 
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
       <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" required ref={emailInputRef} />
+          <label htmlFor='email'>Your Email</label>
+          <input type='email' id='email' required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor="password">Your Password</label>
+          <label htmlFor='password'>Your Password</label>
           <input
-            type="password"
-            id="password"
+            type='password'
+            id='password'
             required
             ref={passwordInputRef}
           />
         </div>
         <div className={classes.actions}>
           {!isLoading && (
-            <button>{isLogin ? "Login" : "Create Account"}</button>
+            <button>{isLogin ? 'Login' : 'Create Account'}</button>
           )}
-          {isLoading && <p>Sending request ...</p>}
+          {isLoading && <p>Sending request...</p>}
           <button
-            type="button"
+            type='button'
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? "Create new account" : "Login with existing account"}
+            {isLogin ? 'Create new account' : 'Login with existing account'}
           </button>
         </div>
       </form>
